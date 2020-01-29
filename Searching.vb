da4f3789f002b99ad377cab1fe9700326da879b7
Alishbaprograms <54945091+Alishbaprograms@users.noreply.github.com>
2020-01-30 01:51:47 +0500

    1.3 membership
    Programming language= Visual Basic

---
+++ Searching.vb
@@ -0,0 +1,37 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim name As String
+        Dim sname As String
+        Dim ID As String
+        Dim IsFound As Boolean
+
+        name = ""
+        sname = ""
+        ID = ""
+        IsFound = False
+
+        Console.WriteLine("Enter the name you are searching for: ")
+        sname = Console.ReadLine
+
+        FileOpen(1, "Membrship.vb", OpenMode.Input)
+        Do
+            Input(1, name)
+            Input(1, ID)
+            If name = sname Then
+                IsFound = True
+                Console.WriteLine("Id: " & ID)
+
+            else 
+            IsFound = False
+            End If
+        Loop Until EOF(1)
+        FileClose(1)
+        If IsFound = False Then
+            Console.WriteLine(sname & "does not exist")
+
+        End If
+
+    End Sub
+
+End Module
